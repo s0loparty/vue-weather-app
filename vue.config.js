ae309f787f8abd20489f27c1227b82dc013a0e64
s0loparty <87995029+s0loparty@users.noreply.github.com>
2021-11-04 18:37:51 +0300

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-	publicPath: process.env.NODE_ENV === 'production' ? '/vue-weather-page/' : '/',
+	// publicPath: process.env.NODE_ENV === 'production' ? '/vue-weather-page/' : '/',
 	productionSourceMap: false,
 }
